42701c35a39b9c650a7d459e86c36e4a48d6adff
john-josue-123 <johnjosuecz19@gmail.com>
2022-11-17 18:15:04 +0000

    se agrega practica_2019

--- flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
+++ flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
@@ -21,7 +21,7 @@ object MakePrediction {
     import spark.implicits._
 
     //Load the arrival delay bucketizer
-    val base_path= "/Users/admin/IdeaProjects/ging/practica_big_data_2019"
+    val base_path= "/home/jchavezz/Desktop/practica_big_data_2019"
     val arrivalBucketizerPath = "%s/models/arrival_bucketizer_2.0.bin".format(base_path)
     print(arrivalBucketizerPath.toString())
     val arrivalBucketizer = Bucketizer.load(arrivalBucketizerPath)
@@ -104,7 +104,7 @@ object MakePrediction {
       concat(
         flightFlattenedDf2("Origin"),
         lit('-'),
-        flightFlattenedDf2("Dest")
+      	  flightFlattenedDf2("Dest")
       )
     )
 
